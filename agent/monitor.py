ec7a731dedad514e8bcd456bea7f47e3ff051725
werayco <werayco@gmail.com>
2025-10-19 14:05:17 -0700

    ai-model-monitoried-using-bertscore

--- agent/monitor.py
+++ agent/monitor.py
@@ -6,23 +6,20 @@ from evidently.descriptors import (
     BERTScore,
     SentenceCount,
 )
-from process import run_agent
+from agent.process import run_agent
 import pandas as pd
 import warnings
-
 warnings.filterwarnings("ignore")
 import time
+import logging
 
 dataframe = pd.read_csv(r"./evals.csv")
 quest = list(dataframe["questions"].to_list())
-# print(quest)
-mdresponse = [run_agent(str(question), int(time.time())) for question in quest]
-dataframe["model_output"] = mdresponse
-print(dataframe)
+ModelResponse = [run_agent(str(question), int(time.time())) for question in quest]
+dataframe["model_output"] = ModelResponse
 
-# print(run_agent(user_input="Who is ryan?", sessionID=89))
-
-# datadef = DataDefinition(text_columns=["questions", "answers", "model_output"])
-# evidentlyAIDataframe = Dataset.from_pandas(dataframe, datadef) # this wraps our dataset to a evidently dataset
-# evidentlyAIDataframe.add_descriptors(descriptors=[BERTScore(columns=["model_output", "answers"])])
-# print(dataframe)
+datadef = DataDefinition(text_columns=["questions", "answers", "model_output"])
+evidentlyAIDataframe = Dataset.from_pandas(dataframe, datadef) # this wraps our dataset to a evidently dataset
+evidentlyAIDataframe.add_descriptors(descriptors=[BERTScore(columns=["model_output", "answers"], alias="BertScore")])
+newdf = evidentlyAIDataframe.as_dataframe().to_csv("./report.csv")
+print(newdf)
